603fa2dfe2adb68d5e0ce1701619ed972011b55f
carbogninalberto <alberto.carbognin@studenti.unitn.it>
2020-08-27 19:03:11 +0200

    webapp

---
+++ src/main/java/com/carbogninalberto/ejb/SubscriptionBean.java
@@ -0,0 +1,2 @@
+package com.carbogninalberto.ejb;public class SubscriptionBean {
+}
--- src/main/webapp/js/controller/dashboard-controller.js
+++ src/main/webapp/js/controller/dashboard-controller.js
@@ -1,6 +1,44 @@
 routerApp.controller('dashboardViewController',
     function($scope, $rootScope) {
         $rootScope.showNavBar = false;
-
         $rootScope.hideAll = !$rootScope.sessionInfo.logged;
+
+        $scope.pages = {
+            general: {
+                profile: {
+                    status: true
+                }
+            },
+            modules: {
+                list: {
+                    status: false
+                },
+                newModule: {
+                    status: false
+                }
+            }
+        };
+
+
+        $scope.resetPage = function () {
+            $scope.pages.general.profile.status = false;
+            $scope.pages.modules.list.status = false;
+            $scope.pages.modules.newModule.status = false;
+        };
+
+        // page is a string value
+        $scope.changePage = function (page) {
+            if (page === 'general.profile') {
+                $scope.resetPage();
+                $scope.pages.general.profile.status = true;
+            } else if (page === 'modules.list') {
+                $scope.resetPage();
+                $scope.pages.modules.list.status = true;
+            } else if (page === 'modules.newModule') {
+                $scope.resetPage();
+                $scope.pages.modules.newModule.status = true;
+            }
+        };
+
+
     });
