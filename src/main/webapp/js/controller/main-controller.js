cfeaefbadc29f03e9cba381476907e7219f92b6c
carbogninalberto <alberto.carbognin@studenti.unitn.it>
2020-08-29 21:13:14 +0200

    Front end add new features

--- src/main/webapp/js/controller/main-controller.js
+++ src/main/webapp/js/controller/main-controller.js
@@ -1,5 +1,33 @@
 routerApp.controller('mainViewController',
-    function($scope, $rootScope) {
+    function($scope, $rootScope, $http, $interval) {
         $rootScope.showNavBar = true;
+        $scope.usage = {
+            value: 0,
+            time: 0
+        };
+
+        $scope.getUsage = function () {
+            $http({
+                method: 'GET',
+                contentType: "application/json; charset=utf-8",
+                url: $rootScope.url + '/AssociazioneSportiva-1.0/memory-usage',
+                processData: false
+            }).then(function success(response) {
+                if (response.status == 200) {
+                    $scope.usage.value = response.data.usage/(1024*1024.0);
+                    $scope.usage.time = response.data.timestamp;
+                } else {
+                    alert("Error on retrive data.");
+                }
+            }, function error(response) {
+                alert("Error on Callback" + response.data.msg);
+            }).catch(function (e) {
+                alert(e);
+            });
+        };
+
+        $interval(function () {
+            $scope.getUsage();
+        }, 2000);
 
     });
--- src/main/webapp/js/controller/subscription-controller.js
+++ src/main/webapp/js/controller/subscription-controller.js
@@ -1,12 +1,54 @@
 routerApp.controller('subscriptionController',
-    function($scope, $rootScope) {
+    function($scope, $rootScope, $http) {
         $rootScope.showNavBar = false;
         $rootScope.hideAll = !$rootScope.sessionInfo.logged;
 
         $scope.subscriptionStatus = 'INFORMATION';
-        $scope.paymentGateway = null;
 
-        // server call
+        $scope.gateways = [
+            { name: "VISA" },
+            { name: "MASTERCARD" },
+            { name: "TRANSFER" }
+        ];
+
+        $scope.paymentGateway = {
+            selected: $scope.gateways[0].name
+        };
+        $scope.subscriptionInfo = {
+            name: "",
+            last: "",
+            date: "",
+            privacy: 0
+        };
+
+        $scope.subscriptions = [];
+
+        $scope.addSubscription = function () {
+            $http({
+                method: 'POST',
+                contentType: "application/json; charset=utf-8",
+                url: $rootScope.url + '/AssociazioneSportiva-1.0/subscription/add',
+                data: {
+                    email: $rootScope.sessionInfo.utente.email,
+                    name: $scope.subscriptionInfo.name,
+                    lastName: $scope.subscriptionInfo.last,
+                    born: $scope.subscriptionInfo.date.getTime()
+                },
+                processData: false
+            }).then(function success(response) {
+                if (response.status == 200) {
+                    $scope.subscriptionStatus = response.data.subscriptionStatus;
+                    $scope.paymentGateway.selected = response.data.paymentGateway;
+                } else {
+                    alert("Error on retrive status of subscription.");
+                }
+            }, function error(response) {
+                alert("Error on Callback" + response.data.msg);
+            }).catch(function (e) {
+                alert(e);
+            });
+        };
+
         $scope.setSubscriptionStatus = function () {
             $http({
                 method: 'POST',
@@ -14,12 +56,13 @@ routerApp.controller('subscriptionController',
                 url: $rootScope.url + '/AssociazioneSportiva-1.0/subscription/status',
                 data: {
                     email: $rootScope.sessionInfo.utente.email,
-                    paymentGateway: $scope.paymentGateway
+                    paymentGateway: $scope.paymentGateway.selected
                 },
                 processData: false
             }).then(function success(response) {
                 if (response.status == 200) {
                     $scope.subscriptionStatus = response.data.subscriptionStatus;
+                    $scope.paymentGateway.selected = response.data.paymentGateway;
                 } else {
                     alert("Error on retrive status of subscription.");
                 }
@@ -28,16 +71,15 @@ routerApp.controller('subscriptionController',
             }).catch(function (e) {
                 alert(e);
             });
-        }
+        };
 
-        $scope.setSubscriptionStatus = function () {
+        $scope.getSubscriptionStatus = function () {
             $http({
                 method: 'GET',
                 contentType: "application/json; charset=utf-8",
                 url: $rootScope.url + '/AssociazioneSportiva-1.0/subscription/status',
-                data: {
-                    email: $rootScope.sessionInfo.utente.email,
-                    paymentGateway: $scope.paymentGateway
+                params: {
+                    email: $rootScope.sessionInfo.utente.email
                 },
                 processData: false
             }).then(function success(response) {
@@ -51,7 +93,29 @@ routerApp.controller('subscriptionController',
             }).catch(function (e) {
                 alert(e);
             });
-        }
+        };
+
+        $scope.getSubscriptions = function () {
+            $http({
+                method: 'GET',
+                contentType: "application/json; charset=utf-8",
+                url: $rootScope.url + '/AssociazioneSportiva-1.0/subscription/list',
+                processData: false
+            }).then(function success(response) {
+                if (response.status == 200) {
+                    $scope.subscriptions = response.data.subscriptions;
+                } else {
+                    alert("Error on retrive subscriptions.");
+                }
+            }, function error(response) {
+                alert("Error on Callback" + response.data.msg);
+            }).catch(function (e) {
+                alert(e);
+            });
+        };
+
+        $scope.getSubscriptions();
+        $scope.getSubscriptionStatus();
 
 
     });
